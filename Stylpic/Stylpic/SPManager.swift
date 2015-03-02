4bf0431e56ee13645f0e4f1143eee987b4a595e8
Neil Bhargava <neilb@email.arizona.edu>
2015-03-01 17:58:33 -0800

    added manager class.

---
+++ Stylpic/Stylpic/SPManager.swift
@@ -0,0 +1,15 @@
+//
+//  SPManager.swift
+//  Stylpic
+//
+//  Created by Neil Bhargava on 3/1/15.
+//  Copyright (c) 2015 Neil Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class SPManager: NSObject {
+   
+    
+    
+}
