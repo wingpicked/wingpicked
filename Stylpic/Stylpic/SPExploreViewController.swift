f7c1628a45330c923f6c8dcb6c63cc7966c67d9a
Joshua Bell <belljh@gmail.com>
2015-03-03 22:56:55 -0800

    updated to save SPPhotos

--- Stylpic/Stylpic/SPExploreViewController.swift
+++ Stylpic/Stylpic/SPExploreViewController.swift
@@ -8,28 +8,20 @@
 
 import UIKit
 
-class SPExploreViewController: UIViewController {
+class SPExploreViewController: UIViewController {//, UITableViewDataSource, UITableViewDelegate {
 
+    var tableViewController : SPPhotosQueryTableViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "tableViewController"){
+            tableViewController = segue.destinationViewController as! SPPhotosQueryTableViewController
+        }
     }
-    */
-
 }
