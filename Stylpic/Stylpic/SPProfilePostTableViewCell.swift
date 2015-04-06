42fe1323e098116d836f2700bc7d4ac430995e60
Neil Bhargava <neilb@email.arizona.edu>
2015-04-05 18:55:26 -0700

    added more button.

--- Stylpic/Stylpic/SPProfilePostTableViewCell.swift
+++ Stylpic/Stylpic/SPProfilePostTableViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class SPProfilePostTableViewCell: SPBaseFeedViewTableViewCell {
     @IBOutlet weak var statsArea: UIView!
 
+    @IBOutlet weak var optionsButton: UIButton!
     //Overrides will go here if any.. currently this implementation matches up exactly with the SPBaseFeedViewTableViewCell...
     override func setupWithFeedItem(feedItem: SPFeedItem){
         super.setupWithFeedItem(feedItem) //Does a lot of heavy lifting that is in common with SPFeedViewTableViewCell and SPProfileFeedTableViewCell
@@ -19,5 +20,8 @@ class SPProfilePostTableViewCell: SPBaseFeedViewTableViewCell {
         let viewingOwnProfile = self.feedItem?.photos?.user.objectId == PFUser.currentUser().objectId
         let hideUI = !viewingOwnProfile && noPhotoLiked
         self.statsArea.hidden = hideUI
+    }
+    @IBAction func optionsButtonTapped(sender: AnyObject) {
+        println("XC0DE SUX0RZ")
     }
 }
