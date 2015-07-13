25708c217615b8b0c6ce3339d6291307e1a71281
Neil Bhargava <neilb@email.arizona.edu>
2015-07-12 22:39:25 -0700

    Add testflightupload script

---
+++ Stylpic/TestflightUpload.sh
@@ -0,0 +1,11 @@
+#Install Shenzhen
+#Install Testflight
+
+# echo "HELLO!"
+# ITUNES_CONNECT_ACCOUNT = "benjam1.bouaziz@gmail.com"
+# echo "HELLO2!"
+# echo "ITUNES_CONNECT_ACCOUNT"
+# ITUNES_CONNECT_PASSWORD = "Moun2067"
+# echo ITUNES_CONNECT_PASSWORD
+ipa build
+ipa distribute:itunesconnect -a benjam1.bouaziz@gmail.com -p Moun2067 -i 1010506731 --upload
