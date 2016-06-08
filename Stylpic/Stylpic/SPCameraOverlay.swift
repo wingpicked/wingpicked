aeea7d22e4ebcefc126a3581b8fa84bf982a446b
Joshua Bell <belljh@gmail.com>
2016-06-07 22:43:01 -0700

    added photos library fetches last photo for camera using this library now in ios 9

--- Stylpic/Stylpic/SPCameraOverlay.swift
+++ Stylpic/Stylpic/SPCameraOverlay.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AssetsLibrary
+import Photos
 
 @objc protocol SPCameraOverlayDelegate {
     func selectPhotosDidTap(overlay: SPCameraOverlay)
@@ -43,7 +44,7 @@ class SPCameraOverlay: UIView {
         self.titleBarView.bringSubviewToFront(self.flashButton)
         self.titleBarView.bringSubviewToFront(self.switchCameraButton)
 //        self.pickingTheLastImageFromThePhotoLibrary()
-        
+        self.photoLibraryButton.contentMode = .ScaleAspectFit
         photoLibraryButton.layer.cornerRadius = 5.0
         photoLibraryButton.clipsToBounds = true
 
@@ -76,49 +77,74 @@ class SPCameraOverlay: UIView {
         self.flashButton.setImage(flashImage, forState: UIControlState.Normal)
     }
     
+    func isIOS9OrHigher() -> Bool {
+        return NSProcessInfo.processInfo().operatingSystemVersion.majorVersion >= 9
+    }
+    
     func pickingTheLastImageFromThePhotoLibrary() {
+        if ( self.isIOS9OrHigher() ) {
+            self.updateLastPhotoThumbnailiOS9()
+        } else {
+            self.updateLastPhotoThumbnailiOS8()
+        }
+        
+        
+    }
+    
+    func updateLastPhotoThumbnailiOS9() {
+        let fetchOptions = PHFetchOptions.init()
+        fetchOptions.sortDescriptors = [NSSortDescriptor.init( key:"creationDate", ascending:true)]
+        let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions )
+        let phAsset = fetchResult.lastObject as! PHAsset
+        PHImageManager.defaultManager().requestImageForAsset(phAsset, targetSize: CGSizeMake(200, 200), contentMode: PHImageContentMode.AspectFit, options: nil) { (imageThumbnail, arrayOfInfo) in
+            self.photoLibraryButton.setImage(imageThumbnail, forState: .Normal)
+        
+        }
+        
+    }
+    
+    func updateLastPhotoThumbnailiOS8() {
         let assetsLibrary: ALAssetsLibrary = ALAssetsLibrary()
-        assetsLibrary.enumerateGroupsWithTypes(ALAssetsGroupSavedPhotos,
-            usingBlock: { (let group: ALAssetsGroup!, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
-                if (group != nil) {
-                    // Be sure to filter the group so you only get photos
-                    group.setAssetsFilter(ALAssetsFilter.allPhotos())
-                    
-                    group.enumerateAssetsWithOptions(NSEnumerationOptions.Reverse,
-                        usingBlock: { (let asset: ALAsset!,
-                            let index: Int,
-                            stop: UnsafeMutablePointer<ObjCBool>)
-                            -> Void in
-                            if(asset != nil) {
-                                /*
-                                Returns a CGImage representation of the asset.
-                                Using the fullResolutionImage uses the original image which — depending on the
-                                device's orientation when taking the photo — could leave you with an upside down,
-                                or -90° image. To get the modified, but optimised image for this, use
-                                fullScreenImage instead.
-                                */
-                                // let myCGImage: CGImage! = asset.defaultRepresentation().fullResolutionImage().takeUnretainedValue()
-                                
-                                /*
-                                Returns a CGImage of the representation that is appropriate for displaying full
-                                screen.
-                                */
-//                                 let myCGImage: CGImage! = asset.defaultRepresentation().fullScreenImage().takeUnretainedValue()
-                                
-                                /* Returns a thumbnail representation of the asset. */
-                                let myCGImage: CGImage! = asset.thumbnail().takeUnretainedValue()
-                                
-                                // Here we set the image included in the UIImageView
-                                self.photoLibraryButton.setImage(UIImage(CGImage: myCGImage), forState: UIControlState.Normal)
-                                stop.memory = ObjCBool(true)
-                            }
-                    })
-                }
+        assetsLibrary.enumerateGroupsWithTypes(ALAssetsGroupSavedPhotos, usingBlock: { (let group: ALAssetsGroup!, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+            if (group != nil) {
+                // Be sure to filter the group so you only get photos
+                group.setAssetsFilter(ALAssetsFilter.allPhotos())
                 
-                stop.memory = ObjCBool(false)
-            })
-            { (let error: NSError!) -> Void in
-                print("A problem occurred: \(error.localizedDescription)")
+                group.enumerateAssetsWithOptions(NSEnumerationOptions.Reverse,
+                    usingBlock: { (let asset: ALAsset!,
+                        let index: Int,
+                        stop: UnsafeMutablePointer<ObjCBool>)
+                        -> Void in
+                        if(asset != nil) {
+                            /*
+                             Returns a CGImage representation of the asset.
+                             Using the fullResolutionImage uses the original image which — depending on the
+                             device's orientation when taking the photo — could leave you with an upside down,
+                             or -90° image. To get the modified, but optimised image for this, use
+                             fullScreenImage instead.
+                             */
+                            // let myCGImage: CGImage! = asset.defaultRepresentation().fullResolutionImage().takeUnretainedValue()
+                            
+                            /*
+                             Returns a CGImage of the representation that is appropriate for displaying full
+                             screen.
+                             */
+                            //                                 let myCGImage: CGImage! = asset.defaultRepresentation().fullScreenImage().takeUnretainedValue()
+                            
+                            /* Returns a thumbnail representation of the asset. */
+                            let thumbnailImage = asset.thumbnail()
+                            let myCGImage: CGImage! = thumbnailImage.takeUnretainedValue()
+                            
+                            // Here we set the image included in the UIImageView
+                            self.photoLibraryButton.setImage(UIImage(CGImage: myCGImage), forState: UIControlState.Normal)
+                            stop.memory = ObjCBool(true)
+                        }
+                })
+            }
+            
+            stop.memory = ObjCBool(false)
+        }) { (let error: NSError!) -> Void in
+            print("A problem occurred: \(error.localizedDescription)")
         }
     }
 }
